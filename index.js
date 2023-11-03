c732c506781a3996beb414b89ede7cf7270cc8e8
Mitchell <mitchell.stockley@gmail.com>
2023-11-03 13:41:16 +0200

    updated

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const cors = require("cors");
-var corsOptions = {
-    origin: "https://silver-daffodil-41ec78.netlify.app/" //will need to be changed for netlify deploy
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 // Body parser
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", auth);
-app.use('*', (req,res) => {
-    res.status(404).json({error: "not found"});
-});
+
 
 // Use the new URL parser and the unified topology options
 mongoose
